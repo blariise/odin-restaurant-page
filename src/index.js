60c12281b428b8bffbe9434077d7b08ba2fb90bf
blarrise <61951559+blariise@users.noreply.github.com>
2025-06-11 21:10:02 +0200

    Add home page and add active button tab

--- src/index.js
+++ src/index.js
@@ -1,4 +1,42 @@
 import "./styles.css";
-import initPage from "./page.js";
+import initHome from "./home.js";
 
-initPage();
+initHome();
+
+function clearContent() {
+  const content_div = document.querySelector("#content");
+  while (content_div.firstChild) {
+    content_div.removeChild(content_div.firstChild);
+  }
+}
+
+function setActiveButton(buttonToActive) {
+  const buttons = document.querySelectorAll("nav button");
+  buttons.forEach( (button) => {
+    button.classList.remove("active");
+  });
+  buttonToActive.classList.add("active");
+}
+
+(() => {
+  const nav = document.querySelector("nav");
+  nav.addEventListener("click", (event) => {
+    const button = event.target;
+    if (button.classList.item(0) === "nav-btn") {
+      setActiveButton(button);
+      clearContent();
+    }
+
+    switch (button.textContent) {
+      case "Home":
+        initHome();
+        break;
+      case "Menu":
+        //initMenu();
+        break;
+      case "About":
+        //initAbout();
+        break;
+    }
+  });
+})();
